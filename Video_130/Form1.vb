ac917ff96c80ef7076b93673268c71d254a66347
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-05 13:13:36 -0500

    Added Finished 130
    Added Finished 130

---
+++ Video_130/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If TextBox1.Text.Length > 0 Then
+            SaveFileDialog1.ShowDialog()
+            System.IO.File.WriteAllText(SaveFileDialog1.FileName, TextBox1.Text)
+        End If
+    End Sub
+End Class
