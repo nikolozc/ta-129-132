9e3d8f435baaccc245bc5c1e1d62744346fdaf32
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-05 13:04:32 -0500

    Finished 129

--- Video_129/Form1.Designer.vb
+++ Video_129/Form1.Designer.vb
@@ -22,17 +22,48 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.lblFile = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.SuspendLayout()
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Location = New System.Drawing.Point(12, 73)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(138, 36)
+        Me.btnOpen.TabIndex = 0
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'lblFile
+        '
+        Me.lblFile.AutoSize = True
+        Me.lblFile.Location = New System.Drawing.Point(12, 41)
+        Me.lblFile.Name = "lblFile"
+        Me.lblFile.Size = New System.Drawing.Size(91, 15)
+        Me.lblFile.TabIndex = 1
+        Me.lblFile.Text = "No File Selected"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(411, 266)
+        Me.ClientSize = New System.Drawing.Size(347, 142)
+        Me.Controls.Add(Me.lblFile)
+        Me.Controls.Add(Me.btnOpen)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents lblFile As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
 End Class
--- Video_129/Form1.vb
+++ Video_129/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
-
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        OpenFileDialog1.Title = "Dialog Title"
+        OpenFileDialog1.ShowDialog()
+        lblFile.Text = OpenFileDialog1.FileName
+    End Sub
 End Class
